b732ad83274b032089e9323d799585d253b259cf
joemalski <joe.malski@gmail.com>
2015-03-28 06:36:37 +0800

    added new files

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+  Package Name: mongoose-bcrypt-compare
+  Dependency Package: bcrypt
+*/
+var bcrypt = require('bcrypt');
+
+var checkEachHash = function(pwdToCompare, hash){
+  var status = '';
+  for(var attr in hash){      
+    status = checkPassword(pwdToCompare, hash[attr].password);
+    if(status == true) 
+      return status;
+    else if(attr >= hash.length - 1){
+      return status;
+    }
+  } 
+}
+
+// private function, compares the specific hash
+function checkPassword(pwdToCompare, hashedPassword){
+  // a lot easier to use than the async version
+  var returnRes = bcrypt.compareSync(pwdToCompare, hashedPassword, function(err, result){
+    if(!err){
+      return result;
+    } else {
+      console.log('bcrypt error: ' + err);
+    }
+  });
+  return returnRes;
+}
+
+module.exports = checkEachHash;
